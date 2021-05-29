e92c9b5d0c2eaa29a3c61bb02254c5d8b5939074
saibe123 <saibekm123@gmail.com>
2021-05-30 00:00:44 +0100

    CHanged processed size again for bugfixing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   def thumb
-    self.image.variant(resize_to_limit: [348,300]).processed
+    self.image.variant(resize_to_limit: [200,200]).processed
   end
 
 
